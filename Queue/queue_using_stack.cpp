10d9a4a647f495602a896b506c061089dfdf332c
Sai Meghana <51068183+yerramsettysaimeghana@users.noreply.github.com>
2020-06-18 20:53:39 +0530

    Update queue_using_stack.cpp

--- Queue/queue_using_stack.cpp
+++ Queue/queue_using_stack.cpp
@@ -27,6 +27,21 @@ struct Queue {
         s2.pop();
         return x;
     }
+    void displayQueue()
+    {
+        cout << "\nDisplaying the queue :-\n";
+        while (!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        while (!s2.empty())
+        {
+            cout << s2.top() << " ";
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
 };
 
 int main()
@@ -34,7 +49,7 @@ int main()
     Queue Q;
     int c,n;
     do{
-        cout<<"\nEnter choice:  1->Enqueue  2->Dequeue 0->Exit\n";
+        cout<<"\nEnter choice:  1->Enqueue  2->Dequeue 3->DisplayQueue 0->Exit\n";
         cin>>c;
         switch(c){
             case 1: cout << "Enter the element to be entered. ";
@@ -43,6 +58,9 @@ int main()
                     break;
 
             case 2: cout<<Q.deQueue();
+                    break;
+                
+            case 3: Q.displayQueue();
                     break;
 
             case 0: break;
